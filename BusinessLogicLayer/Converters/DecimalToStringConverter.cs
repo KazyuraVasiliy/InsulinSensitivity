b902e46c330c8940c505b13bc8785d02c54821cf
Василий Казюра <kazyuravasiliy@gmail.com>
2020-12-21 21:01:27 +0700

    Добавлен converter для ввода decimal; добавлен converter для инвертирования bool

---
+++ BusinessLogicLayer/Converters/DecimalToStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BusinessLogicLayer.Converters
+{
+    public class DecimalToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value?.ToString()
+                .Replace(".", CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator)
+                .Replace(",", CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            string stringValue = (value as string)
+                ?.Replace(".", CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator)
+                ?.Replace(",", CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator);
+
+            if ((stringValue?.Length ?? 0) > 0 && stringValue[stringValue.Length - 1].ToString() == CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator)
+                return Binding.DoNothing;
+
+            if (decimal.TryParse(stringValue, NumberStyles.Number, CultureInfo.CurrentUICulture, out decimal result))
+                return result;
+            return null;
+        }
+    }
+}
