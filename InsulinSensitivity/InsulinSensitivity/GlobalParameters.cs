68dccedc922188e23c5d92bcf7f7a8f8fca3dcf7
Василий Казюра <kazyuravasiliy@gmail.com>
2020-12-25 21:36:36 +0700

    User, DbPath, Navigation вынесены в static class

---
+++ InsulinSensitivity/InsulinSensitivity/GlobalParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Models = DataAccessLayer.Models;
+
+namespace InsulinSensitivity
+{
+    public static class GlobalParameters
+    {
+        /// <summary>
+        /// Текущий пользователь
+        /// </summary>
+        public static Models.User User { get; set; }
+
+        /// <summary>
+        /// Путь до БД
+        /// </summary>
+        public static string DbPath { get; set; }
+
+        /// <summary>
+        /// Навигация
+        /// </summary>
+        public static INavigation Navigation { get; set; }
+    }
+}
