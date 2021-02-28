e85e89bf8bc94e41ed20fd2802679115b22ac848
Василий Казюра <kazyuravasiliy@gmail.com>
2021-02-28 15:30:46 +0700

    Настройки. Пересчёт УК, БК, ЖК при учёте базального инсулина

--- InsulinSensitivity/InsulinSensitivity/Settings/SettingsPageViewModel.cs
+++ InsulinSensitivity/InsulinSensitivity/Settings/SettingsPageViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.ObjectModel;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 using DataAccessLayer.Contexts;
 using BusinessLogicLayer.ViewModel;
 using BusinessLogicLayer.Service;
 using Models = DataAccessLayer.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace InsulinSensitivity.Settings
 {
@@ -22,6 +27,37 @@ namespace InsulinSensitivity.Settings
             get => GlobalParameters.Settings.IsActiveBasal;
             set
             {
+                bool question = GlobalParameters.Navigation.NavigationStack.Last().DisplayAlert(
+                    "Запрос",
+                    "Пересчитать пищевые коэффициенты при переходе на новую формулу расчёта?",
+                    "Да",
+                    "Нет").Result;
+
+                if (question)
+                {
+                    using (var db = new ApplicationContext(GlobalParameters.DbPath))
+                    {
+                        var entity = db.Users.Find(GlobalParameters.User.Id);
+                        if (entity != null)
+                        {
+                            if (value)
+                            {
+                                entity.CarbohydrateCoefficient *= 1.15M;
+                                entity.ProteinCoefficient /= 1.15M;
+                                entity.FatCoefficient /= 1.15M;
+                            }
+                            else
+                            {
+                                entity.CarbohydrateCoefficient /= 1.15M;
+                                entity.ProteinCoefficient *= 1.15M;
+                                entity.FatCoefficient *= 1.15M;
+                            }
+
+                            db.SaveChanges();
+                        }
+                    }
+                }
+
                 GlobalParameters.Settings.IsActiveBasal = value;
                 OnPropertyChanged();
             }
