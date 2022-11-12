5ed3e9c813936023df555c0f9b493faf169a4027
Василий Казюра <kazyuravasiliy@gmail.com>
2022-11-12 18:16:19 +0700

    Имя версии изменено на 3.3

--- InsulinSensitivity/InsulinSensitivity.Android/Resources/Resource.designer.cs
+++ InsulinSensitivity/InsulinSensitivity.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace InsulinSensitivity.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.99.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.0.0.73")]
 	public partial class Resource
 	{
 		
