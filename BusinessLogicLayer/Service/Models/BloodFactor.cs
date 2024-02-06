8cf04404dfe798ed647804bcb7e81375c3ef341b
Василий Казюра <kazyuravasiliy@gmail.com>
2024-02-06 20:07:37 +0700

    Информация. Добавлена расширенная документация

---
+++ BusinessLogicLayer/Service/Models/BloodFactor.cs
@@ -0,0 +1,40 @@
+﻿namespace BusinessLogicLayer.Service.Models
+{
+    public class BloodFactor
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="age"></param>
+        /// <param name="imt"></param>
+        /// <param name="man"></param>
+        /// <param name="woman"></param>
+        public BloodFactor(string age, string imt, string man, string woman)
+        {
+            Age = age;
+            IMT = imt;
+            Man = man;
+            Woman = woman;
+        }
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public string Age { get; set; }
+
+        /// <summary>
+        /// ИМТ
+        /// </summary>
+        public string IMT { get; set; }
+
+        /// <summary>
+        /// Мужчина
+        /// </summary>
+        public string Man {  get; set; }
+
+        /// <summary>
+        /// Женщина
+        /// </summary>
+        public string Woman { get; set; }
+    }
+}
--- InsulinSensitivity/InsulinSensitivity/Information/InformationPageViewModel.cs
+++ InsulinSensitivity/InsulinSensitivity/Information/InformationPageViewModel.cs
@@ -1,21 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Windows.Input;
-using Xamarin.Forms;
-
-using DataAccessLayer.Contexts;
+﻿using BusinessLogicLayer.Service.Models;
 using BusinessLogicLayer.ViewModel;
-using BusinessLogicLayer.Service;
-using Models = DataAccessLayer.Models;
-using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace InsulinSensitivity.Information
 {
     public class InformationPageViewModel : ObservableBase
     {
-
+        public List<BloodFactor> BloodFactors =>
+            new List<BloodFactor>()
+            {
+                new BloodFactor("Возраст", "ИМТ", "Мужчина", "Женщина"),
+                new BloodFactor("< 6 мес.", "-", "87", "87"),
+                new BloodFactor("< 1 года", "-", "86", "86"),
+                new BloodFactor("< 6 лет", "-", "80", "80"),
+                new BloodFactor("< 7 лет", "-", "78", "78"),
+                new BloodFactor("< 8 лет", "-", "77", "77"),
+                new BloodFactor("< 9 лет", "-", "76", "76"),
+                new BloodFactor("< 10 лет", "-", "75", "75"),
+                new BloodFactor("< 11 лет", "-", "74", "74"),
+                new BloodFactor("< 12 лет", "-", "73", "73"),
+                new BloodFactor("< 13 лет", "-", "72", "72"),
+                new BloodFactor("< 18 лет", "-", "71", "71"),
+                new BloodFactor(">= 18 лет", ">= 30", "60", "55"),
+                new BloodFactor(">= 18 лет", "20 < x < 25", "70", "65"),
+                new BloodFactor(">= 18 лет", "Иначе", "65", "60"),
+            };
     }
 }
